b2c23e2d4e81ad6b019545f60b919525356ce5a5
Daniel Bielaszka <danielbielaszka@gmail.com>
2024-04-29 18:10:13 +0200

    refactor: simplify UserViewSet

--- Server/homelink/database/urls.py
+++ Server/homelink/database/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import UserViewSet
+from .views import *
+
 
 urlpatterns = [
-    path("user/add/", UserViewSet.as_view({"post": "create_user"})),
+    path("user/add/", UserViewSet.as_view({"post": "create"})),
+    path("user/get/<int:pk>/", UserViewSet.as_view({"get": "retrieve"})),
 ]
--- Server/homelink/database/views.py
+++ Server/homelink/database/views.py
@@ -1,18 +1,9 @@
 from django.contrib.auth.models import User
-
-from rest_framework.response import Response
-from rest_framework import status
 from rest_framework import viewsets
+from .models import *
+from .serializers import *
 
 
 class UserViewSet(viewsets.ModelViewSet):
-
-    def create_user(self, request):
-        try:
-            data = request.data
-            user = User.objects.create_user(
-                username=data["username"], password=data["password"]
-            )
-            return Response({"user_id": user.id}, status=status.HTTP_201_CREATED)
-        except Exception as e:
-            return Response(str(e), status=status.HTTP_400_BAD_REQUEST)
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
