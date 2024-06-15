d1a497a5b3bbcf8c25131998f62c968440871168
Daniel Bielaszka <danielbielaszka@gmail.com>
2024-06-15 23:38:42 +0200

    feat: add function to get JSON response for a given prompt in database app

---
+++ Server/homelink/database/llm.py
@@ -0,0 +1,26 @@
+import os
+
+from openai import OpenAI
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+MODEL = os.getenv("MODEL")
+
+if not OPENAI_API_KEY or not MODEL:
+    raise Exception("OpenAI API Key and Model must be set in .env file")
+
+client = OpenAI(api_key=OPENAI_API_KEY)
+
+
+def get_response(prompt):
+    response = client.chat.completions.create(
+        model=MODEL,
+        messages=[
+            {"role": "system", "content": "Return ONLY VALID JSON"},
+            {"role": "user", "content": prompt},
+        ],
+    )
+    return response.choices[0].message
