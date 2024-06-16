100f0e5ce5bd496001ef603781f60c2072cb7504
Daniel Bielaszka <danielbielaszka@gmail.com>
2024-06-16 15:15:07 +0200

    fix: queryset lookup for CommandViewSet

--- Server/homelink/database/views.py
+++ Server/homelink/database/views.py
@@ -5,25 +5,28 @@ from .serializers import *
 from rest_framework import viewsets, status
 from rest_framework.response import Response
 
+
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    
+
 
 class SpaceDevicesView(viewsets.ModelViewSet):
     serializer_class = DeviceSerializer
 
     def get_queryset(self):
-        space_id = self.kwargs['space_id']
+        space_id = self.kwargs["space_id"]
         return Device.objects.filter(space_id=space_id)
 
     def perform_create(self, serializer):
-        space_id = self.kwargs['space_id']
+        space_id = self.kwargs["space_id"]
         try:
             space = Space.objects.get(id=space_id)
             serializer.save(space=space, owner=self.request.user)
         except Space.DoesNotExist:
-            return Response({'error': 'Space not found'}, status=status.HTTP_404_NOT_FOUND)
+            return Response(
+                {"error": "Space not found"}, status=status.HTTP_404_NOT_FOUND
+            )
 
 
 class DeviceViewSet(viewsets.ModelViewSet):
@@ -33,7 +36,7 @@ class DeviceViewSet(viewsets.ModelViewSet):
         return Device.objects.filter(owner=self.request.user)
 
     def perform_create(self, serializer):
-        space_id = self.request.data.get('space_id')
+        space_id = self.request.data.get("space_id")
         print(f"Received space_id: {space_id}")  # Log the space_id
 
         try:
@@ -42,10 +45,14 @@ class DeviceViewSet(viewsets.ModelViewSet):
             serializer.save(owner=self.request.user)
         except Space.DoesNotExist:
             print(f"Space with id {space_id} does not exist")  # Log the error
-            return Response({'error': 'Invalid space ID'}, status=status.HTTP_400_BAD_REQUEST)
+            return Response(
+                {"error": "Invalid space ID"}, status=status.HTTP_400_BAD_REQUEST
+            )
         except Exception as e:
             print(f"An error occurred: {e}")  # Log any other exceptions
-            return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+            return Response(
+                {"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR
+            )
 
 
 class SpaceViewSet(viewsets.ModelViewSet):
@@ -57,11 +64,12 @@ class SpaceViewSet(viewsets.ModelViewSet):
     def perform_create(self, serializer):
         serializer.save()
 
+
 class CommandViewSet(viewsets.ModelViewSet):
     serializer_class = CommandSerializer
 
     def get_queryset(self):
-        return Command.objects.filter(device__owner=self.request.user)
+        return Command.objects.filter(devices__owner=self.request.user)
 
     def perform_create(self, serializer):
         serializer.save()
