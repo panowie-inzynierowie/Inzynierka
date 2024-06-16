7bc8803972fcb934b5717821cde91d569030defb
Daniel Bielaszka <danielbielaszka@gmail.com>
2024-06-16 16:35:28 +0200

    feat: add related_name for owner on Device model

--- Server/homelink/database/models.py
+++ Server/homelink/database/models.py
@@ -21,11 +21,11 @@ class Device(models.Model):
 
     added_at = models.DateTimeField(auto_now_add=True)
 
-    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="devices")
     space = models.ForeignKey(Space, on_delete=models.SET_NULL, null=True, blank=True)
 
     def __str__(self):
-        return f"{self.name} {self.owner.username}"
+        return f"{self.name} | {self.owner.username}"
 
 
 class Command(models.Model):
