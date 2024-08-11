27b4fbec20de9f058988d747f16b778b5b775205
Daniel Bielaszka <danielbielaszka@gmail.com>
2024-08-11 17:59:17 +0200

    feat: add speech recognition as option for message input

--- Client/lib/chat.dart
+++ Client/lib/chat.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 import 'package:provider/provider.dart';
+import 'package:speech_to_text/speech_to_text.dart';
+import 'package:speech_to_text/speech_recognition_result.dart';
 
 import 'package:inzynierka_client/classes/chat.dart';
 import 'package:inzynierka_client/state/state.dart';
@@ -16,10 +18,41 @@ class ChatDialogState extends State<ChatDialog> {
   List<ChatMessage> messages = [];
   final fieldText = TextEditingController();
 
+  final SpeechToText _speechToText = SpeechToText();
+  bool _listening = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _initSpeech();
+  }
+
+  void _initSpeech() async {
+    await _speechToText.initialize();
+  }
+
   @override
   void didChangeDependencies() {
     super.didChangeDependencies();
     messages = context.watch<AppState>().chatMessages;
+  }
+
+  void _startListening() async {
+    setState(() {
+      _listening = true;
+    });
+    await _speechToText.listen(onResult: _onSpeechResult);
+  }
+
+  void _stopListening() async {
+    setState(() {
+      _listening = false;
+    });
+    await _speechToText.stop();
+  }
+
+  void _onSpeechResult(SpeechRecognitionResult result) {
+    fieldText.value = TextEditingValue(text: result.recognizedWords);
   }
 
   @override
@@ -66,21 +99,42 @@ class ChatDialogState extends State<ChatDialog> {
               },
             ),
           ),
-          TextFormField(
-            controller: fieldText,
-            decoration: const InputDecoration(
-              labelText: 'New message',
+          SizedBox(
+            width: double.infinity,
+            child: Row(
+              children: [
+                Expanded(
+                  child: TextFormField(
+                    controller: fieldText,
+                    decoration: const InputDecoration(
+                      labelText: 'New message',
+                    ),
+                    onFieldSubmitted: (value) {
+                      setState(
+                        () {
+                          messages.add(
+                              ChatMessage(author: Author.user, content: value));
+                        },
+                      );
+                      fieldText.clear();
+                    },
+                  ),
+                ),
+                IconButton(
+                  icon: _listening
+                      ? const Icon(Icons.mic_off)
+                      : const Icon(Icons.mic),
+                  onPressed: () {
+                    if (_speechToText.isListening) {
+                      _stopListening();
+                    } else {
+                      _startListening();
+                    }
+                  },
+                )
+              ],
             ),
-            onFieldSubmitted: (value) {
-              setState(
-                () {
-                  messages
-                      .add(ChatMessage(author: Author.user, content: value));
-                },
-              );
-              fieldText.clear();
-            },
-          ),
+          )
         ],
       ),
     );
