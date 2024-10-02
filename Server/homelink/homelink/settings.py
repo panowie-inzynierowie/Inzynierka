45b0509968f19163abbef8173c868da6d7db5379
Daniel Bielaszka <danielbielaszka@gmail.com>
2024-10-03 00:29:32 +0200

    feat: switch to postgresql

--- Server/homelink/homelink/settings.py
+++ Server/homelink/homelink/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = "homelink.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "homelink",
+        "USER": "postgres",
+        "PASSWORD": "password",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
