7095972776d9541b8ae4b115e3b465c8f46b0147
Daniel Bielaszka <danielbielaszka@gmail.com>
2024-11-05 20:05:10 +0100

    feat: add get_user_devices method to get user's devices and devices from user's spaces

--- Server/homelink/database/models/auth.py
+++ Server/homelink/database/models/auth.py
@@ -1,7 +1,19 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.apps import apps
 
 
 class CustomUser(AbstractUser):
     is_device = models.BooleanField(default=False)
     owner = models.ForeignKey("self", on_delete=models.CASCADE, null=True, blank=True)
+
+    def get_user_devices(self):
+        Device = apps.get_model("database", "Device")
+        Space = apps.get_model("database", "Space")
+
+        result = list(Device.objects.filter(owner=self))
+        for s in Space.objects.filter(users=self):
+            for d in s.device_set.all():
+                result.append(d)
+
+        return list(set(result))
