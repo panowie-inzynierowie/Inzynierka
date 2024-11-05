358e4ea40681b64cb8b67e1e25f4bfbfc8cdfa56
Daniel Bielaszka <danielbielaszka@gmail.com>
2024-11-05 20:08:32 +0100

    refactor: remove unused code

--- Server/homelink/database/serializers.py
+++ Server/homelink/database/serializers.py
@@ -3,7 +3,6 @@ import json
 from rest_framework import serializers
 
 from .models.models import *
-from .llm import get_structured_response, CommandsResponse
 
 
 User = get_user_model()
@@ -52,31 +51,6 @@ class CommandSerializer(serializers.ModelSerializer):
     class Meta:
         model = Command
         fields = ["id", "description", "scheduled_at", "device", "data", "self_execute"]
-
-
-def create_command(validated_data, device):
-    devices_data = [
-        {"name": d.name, "description": d.description, "data": d.data}
-        for d in validated_data["owner"].devices.all()
-        if d.pk != device.pk
-    ]
-
-    prompt = (
-        f"These are available devices with their descriptions and data: {json.dumps(devices_data)}"
-        f"New device is being added, with data: {json.dumps({'name': validated_data['name'], 'description': validated_data['description']})} "
-        f"Can you create any commands based on current devices and new device being added?"
-        "Command is an action that should be triggered in case of any event that matches criteria described in descriptions of devices provided by user. "
-        "Return possible commands."
-    )
-
-    if data := get_structured_response(prompt, CommandsResponse):
-        data: CommandsResponse
-        for command in data.commands:
-            c = Command.objects.create(
-                author=validated_data["owner"],
-                description=f"{command.triggering_action} -> {command.action_to_perform} ({command.device_affected})",
-            )
-            c.devices.add(device)
 
 
 class CommandsLinkSerializer(serializers.ModelSerializer):
